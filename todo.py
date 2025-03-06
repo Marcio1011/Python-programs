02d47b4979b67b05ec2e5487baea0efd2e2f623b
Marcio Rosenberg <marcio101167@gmail.com>
2025-03-06 11:41:52 +0200

    updated program

--- todo.py
+++ todo.py
@@ -1,3 +1,7 @@
-print("Enter Todo:")
-user_text=input()
-print(user_text)
+user_prompt = "Enter a todo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
