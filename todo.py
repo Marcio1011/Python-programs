d1a66629cc6f6c62b82363608ace3073899315cd
Marcio Rosenberg <marcio101167@gmail.com>
2025-03-27 14:43:33 +0200

    code changes

--- todo.py
+++ todo.py
@@ -1,14 +1,14 @@
 #Initial user prompt
 user_prompt = "Enter a todo:"
-#First user prompt
-todo1 = input(user_prompt)
-#Second user prompt
-todo2 = input(user_prompt)
-#Third user prompt
-todo3 = input(user_prompt)
-#A list which holds all the different user prompts values
-todos = [todo1, todo2, todo3, "Hello"]
-#print the list values
-print(todos)
 
-print(type(todo1))
+#Create an empty list
+todos = []
+
+#While loop
+while True:
+    #Gets the user input
+    todo = input(user_prompt)
+    #Append the input in the list
+    todos.append(todo)
+    #Prints the list
+    print(todos)
