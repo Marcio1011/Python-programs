43e669e4afe02e6e1eb9f95920f28e1fdcbe4ad8
Marcio1011 <marcio101167@gmail.com>
2025-04-29 14:56:23 +0300

    Upload a new python program

---
+++ Numbers multiplication.py
@@ -0,0 +1,5 @@
+#This program accepts 2 numbers from the user, multiply them and print the result
+first_number = int(input("Type the first number: ")) #Asks the user to write the first number and turns it to an integer
+second_number = int(input("Type the second number: ")) #Asks the user to write the second number and turns it to an integer
+result = first_number * second_number #multiply the 2 numbers
+print("The result is:",result) #prints the result
