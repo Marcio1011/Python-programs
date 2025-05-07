d993b1084a50b5e11fa0f9245e7bda4a8ba5a041
Marcio1011 <marcio101167@gmail.com>
2025-05-07 14:01:43 +0300

    Add files via upload

---
+++ Display decimal to octal using the print function.py
@@ -0,0 +1,3 @@
+#This program gets an integer in base 10 and converts it to octal base
+user_input = int(input("input the number to be converted to octal base: ")) #converting the user input from string to integer
+print("%o" % user_input)
