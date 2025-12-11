f23717a99266432008d5fe1efc805319da8ca62a
Marcio <marcio101167@gmail.com>
2025-12-11 12:41:53 +0200

    Cast

---
+++ cast.py
@@ -0,0 +1,10 @@
+"""
+This program reads the first number from input and store it in a variable
+Then it reads the second number from input and store it in another variable
+It converts both variables to float type (for decimal numbers) and calculate the multiplication of these two numbers
+and print the result
+"""
+var1 = float(input())
+var2 = float(input())
+var3 = var1 * var2
+print(var3)
